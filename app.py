67294c9ddf88fbab3f79de2646aa1ae3a40c2866
Mehfooz Ali <graphicx.mehfooz201@gmail.com>
2023-04-25 00:02:47 +0500

    updated_openCv_code

--- app.py
+++ app.py
@@ -1,29 +1,81 @@
 from flask import Flask, render_template, request, redirect, Response, url_for
+import face_recognition 
 import cv2
+import numpy as np
 
 app = Flask(__name__)
 camera = cv2.VideoCapture(0)
 
+#Load a sample picture and learn how to recognize it.
+mehfooz_img = face_recognition.load_image_file("mehfooz/mehfooz.jpg")
+mehfooz_face_encoding = face_recognition.face_encodings(mehfooz_img)[0]
+
+# Load a second sample picture and learn how to recognize it.
+bradly_image = face_recognition.load_image_file("bradly/Bradley-cooper-300.jpg")
+bradly_face_encoding = face_recognition.face_encodings(bradly_image)[0]
+
+# Create arrays of known face encodings and their names
+known_face_encodings = [
+    mehfooz_face_encoding,
+    bradly_face_encoding
+]
+known_face_names = [
+    "Mehfooz Ali",
+    "Bradley Cooper"
+]
+
+# Initialize some variables
+face_locations = []
+face_encodings = []
+face_names = []
+process_this_frame = True
+
 
 def generate_frames():
     while True:
-        ##read the Camera Frame
-        success,frame = camera.read()
+        success, frame = camera.read()  # read the camera frame
         if not success:
             break
         else:
-            detector=cv2.CascadeClassifier('harcascade/haarcascade_frontalface_default.xml')
-            eye_cascade = cv2.CascadeClassifier('harcascade/haarcascade_eye.xml')
-            faces=detector.detectMultiScale(frame,1.1,7)
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-             #Draw the rectangle around each face
-            for (x, y, w, h) in faces:
-                cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-                roi_gray = gray[y:y+h, x:x+w]
-                roi_color = frame[y:y+h, x:x+w]
-                eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 3)
-                for (ex, ey, ew, eh) in eyes:
-                    cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+            # Resize frame of video to 1/4 size for faster face recognition processing
+            small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+            # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+            rgb_small_frame = small_frame[:, :, ::-1]
+
+            # Only process every other frame of video to save time
+           
+            # Find all the faces and face encodings in the current frame of video
+            face_locations = face_recognition.face_locations(rgb_small_frame)
+            face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+            face_names = []
+            for face_encoding in face_encodings:
+                # See if the face is a match for the known face(s)
+                matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+                name = "Unknown"
+                # Or instead, use the known face with the smallest distance to the new face
+                face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+                best_match_index = np.argmin(face_distances)
+                if matches[best_match_index]:
+                    name = known_face_names[best_match_index]
+
+                face_names.append(name)
+            
+
+            # Display the results
+            for (top, right, bottom, left), name in zip(face_locations, face_names):
+                # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+                top *= 4
+                right *= 4
+                bottom *= 4
+                left *= 4
+
+                # Draw a box around the face
+                cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+                # Draw a label with a name below the face
+                cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+                font = cv2.FONT_HERSHEY_DUPLEX
+                cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
             ret, buffer = cv2.imencode('.jpg', frame)
             frame = buffer.tobytes()
